d21ca979b3b1c15cf8fae27ff1e7d57b60d18ecf
Vladimir Savenkov <vladimir.savenkov@home24.de>
2015-12-23 15:20:14 +0100

    Extended reporting for HTTPProxy

--- src/processor/HttpProxy.go
+++ src/processor/HttpProxy.go
@@ -29,29 +29,38 @@ func (h *HTTPProxy) Process(job qp.IJob) error {
 	h.logger.WithField("job", job).Debug("Processing job")
 	serializedMessage, err := job.GetMessage().Serialize()
 	if err != nil {
-		h.logger.WithField("error", err).Error("Error during message serialization")
+		h.logger.WithError(err).Error("Error during message serialization")
 		return err
 	}
 
 	request, err := http.NewRequest("POST", h.configuration.URL, strings.NewReader(serializedMessage))
 	if err != nil {
+		h.logger.WithError(err).Warn("Error sending HTTP request")
 		return err
 	}
 
 	resp, err := h.client.Do(request)
-	if err != nil || resp.StatusCode != 200 {
+
+	if err == nil && resp.StatusCode != 200 {
+		err = errors.New("Response code is not 200. It is: "+resp.Status)
+	}
+
+	if err != nil  {
+		h.logger.WithError(err).Debug("Job failed")
 		if rejectError := job.RejectMessage(); rejectError != nil {
-			h.logger.WithField("error", rejectError).Debug("Error on MessageReject")
+			h.logger.WithError(rejectError).Debug("Error on MessageReject")
 			return rejectError
+		} else {
+			h.logger.Debug("Job rejected")
 		}
-		h.logger.Debug("job rejected")
 	} else {
 		resp.Body.Close()
+		h.logger.Debug("Job Processed")
 		if ackError := job.AckMessage(); ackError != nil {
-			h.logger.WithField("error", ackError).Debug("Error on MessageAcknowledge")
+			h.logger.WithError(ackError).Debug("Error on MessageAcknowledge")
 			return ackError
 		}
-		h.logger.Debug("job acknowledged")
+		h.logger.Debug("Job acknowledged")
 	}
 
 	return nil
